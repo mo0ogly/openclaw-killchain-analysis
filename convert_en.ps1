8ba8b8d682c16e71884290468a622fe568de68f1
mo0ogly <mo0ogly@users.noreply.github.com>
2026-02-21 22:13:24 +0100

    fix : sup ps1 file

--- convert_en.ps1
+++
@@ -1,6 +0,0 @@
-$word = New-Object -ComObject Word.Application
-$doc = $word.Documents.Open("C:\Users\pizzif\Documents\GitHub\openclaw-killchain-analysis\S1-ISI5_AI_and_Cybersecurity_v6_EN.docx")
-$path = "C:\Users\pizzif\Documents\GitHub\openclaw-killchain-analysis\S1-ISI5_IA_et_Cybersecurite v6_en.pdf"
-$doc.SaveAs([ref] $path, [ref] 17)
-$doc.Close()
-$word.Quit()
